3de543fb76d139f9b6530051788305a447d24ee6
ryo oomichi <oomichi@na-s.jp>
2020-10-27 10:35:06 +0900

    section 8

--- Landmarks/LandmarkList.swift
+++ Landmarks/LandmarkList.swift
@@ -22,6 +22,10 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkList()
+        ForEach(["iPhone SE", "iPhone 11", "iPad Pro (9.7-inch)"],  id: \.self) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
     }
 }
